883bfe97d9673df328f522d63d92dc09ce191a5b
Advaith B G <hereliesadvaith@gmail.com>
2024-08-05 23:59:35 +0530

    [ADD] expense model

--- api/models.py
+++ api/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Expense(models.Model):
+    """
+    Model for expense table
+    """
+    expense = models.CharField(max_length=20)
+    price = models.FloatField()
+    date = models.DateField()
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """
+        String Representation
+        """
+        return f"{self.expense}: {self.price}"
