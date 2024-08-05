df30821b26e18a4fa078fa912c5ea5ce794a8fa5
Advaith B G <hereliesadvaith@gmail.com>
2024-08-06 01:29:22 +0530

    [ADD] django-owl connection

--- quick_budget/settings.py
+++ quick_budget/settings.py
@@ -61,7 +61,9 @@ ROOT_URLCONF = 'quick_budget.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'frontend'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'frontend/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
