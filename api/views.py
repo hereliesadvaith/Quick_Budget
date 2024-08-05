4361ee3f241e2507b57419779492fe13ffbb1f12
Advaith B G <hereliesadvaith@gmail.com>
2024-08-05 23:35:26 +0530

    [ADD] api endpoints

--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def get_routes(request):
+    routes = [
+        {
+            'Endpoint': '/expense/',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of expenses'
+        },
+        {
+            'Endpoint': '/expense/create/',
+            'method': 'POST',
+            'body': {'body': ""},
+            'description': 'Creates new expense with data sent in post request'
+        },
+    ]
+    return JsonResponse(routes, safe=False)
