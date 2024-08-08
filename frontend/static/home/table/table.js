2c786e1c96eed197d00d6ecfd3051fe80928d4b4
Advaith B G <hereliesadvaith@gmail.com>
2024-08-08 23:50:02 +0530

    [ADD] postgresql

--- frontend/static/home/table/table.js
+++ frontend/static/home/table/table.js
@@ -7,7 +7,8 @@ export class Table extends Component {
 
     setup() {
         this.state = useState({
-            expenses: []
+            expenses: [],
+            edit_line: 0
         })
         // Will start hook can be used to perform to load external assets, it will run just before the initial rendering.
         onWillStart(async () => {
@@ -17,11 +18,11 @@ export class Table extends Component {
 
     async loadExpenses() {
         let response = await fetch('/api/expense/')
-        this.state.expenses = await response.json()      
+        this.state.expenses = await response.json()  
     }
 
     onClickEdit(ev) {
-        console.log(ev)
+        this.state.edit_line = ev.currentTarget.dataset['oe_id']
     }
 
     onClickDelete(ev) {
@@ -49,14 +50,24 @@ export class Table extends Component {
                 </thead>
                 <tbody>
                     <t t-foreach="state.expenses" t-as="expense" t-key="expense.id">
-                        <tr>
-                            <td t-esc="expense.date"/>
-                            <td t-esc="expense.expense"/>
-                            <td t-esc="expense.price"/>
-                            <td t-esc="expense.categroy"/>
-                            <td style="width: 5%;"><i class="fa fa-edit text-warning" style="cursor: pointer;" t-on-click="onClickEdit"></i></td>
+                        <t t-if="expense.id == state.edit_line">
+                            <td><input type="date" t-model="expense.date" class="form-control"/></td>
+                            <td><input type="text" t-model="expense.expense" class="form-control"/></td>
+                            <td><input type="number" t-model="expense.price" class="form-control"/></td>
+                            <td><input type="text" t-model="expense.category" class="form-control"/></td>
+                            <td style="width: 5%;"><i class="fa fa-check text-warning" style="cursor: pointer;" t-on-click="onClickEdit"></i></td>
                             <td style="width: 5%;"><i class="fa fa-trash text-danger" style="cursor: pointer;" t-on-click="onClickDelete"></i></td>
-                        </tr>
+                        </t>
+                        <t t-else="">
+                            <tr>
+                                <td t-esc="expense.date"/>
+                                <td t-esc="expense.expense"/>
+                                <td t-esc="expense.price"/>
+                                <td t-esc="expense.categroy"/>
+                                <td style="width: 5%;"><i t-att-data-oe_id="expense.id" class="fa fa-edit text-warning" style="cursor: pointer;" t-on-click="onClickEdit"></i></td>
+                                <td style="width: 5%;"><i t-att-data-oe_id="expense.id" class="fa fa-trash text-danger" style="cursor: pointer;" t-on-click="onClickDelete"></i></td>
+                            </tr>
+                        </t>
                     </t>
                 </tbody>
             </table>
--- quick_budget/settings.py
+++ quick_budget/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -84,8 +87,12 @@ WSGI_APPLICATION = 'quick_budget.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
@@ -135,4 +142,4 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_ALL_ORIGINS = False
