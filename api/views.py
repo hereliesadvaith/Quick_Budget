7a64d22d130ef3aa50b973a2950fb0cb02826e5e
Advaith B G <hereliesadvaith@gmail.com>
2024-09-17 21:49:43 +0530

    [ADD] dynamic serializer class

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
-from .serializers import ExpenseSerializer
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
+from importlib import import_module
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -37,11 +37,17 @@ def orm(request):
     REST API CRUD Methods
     """
     user = request.user
-    if request.method == 'GET':
-        data = request.GET
-        content_type = ContentType.objects.get(model=data.get('model'))
-        model_class = content_type.model_class()
-        records = model_class.objects.filter(user=user).order_by(
-            'date', 'created').reverse()[:10]
-        serializer = ExpenseSerializer(records, many=True)
-        return Response(serializer.data)
+    match request.method:
+        case 'GET':
+            data = request.GET
+            content_type = ContentType.objects.get(model=data.get('model'))
+            model_class = content_type.model_class()
+            serializer_class = getattr(
+                import_module('api.serializers'),
+                data.get('model').capitalize() + 'Serializer'
+            )
+            records = model_class.objects.filter(user=user).order_by(
+                'date', 'created').reverse()[:10]
+            serializer = serializer_class(records, many=True)
+            return Response(serializer.data)
+    
